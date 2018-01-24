79c0bda954cbd2895192966a535ed9898328ec4f
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-01-24 13:55:51 +0100

    Correction config globale UnicaenApp: Sodoct=>SYGAL

--- config/autoload/unicaen-app.global.php
+++ config/autoload/unicaen-app.global.php
@@ -1,35 +1,26 @@
 <?php
-/**
- * Configuration globale du module UnicaenApp.
- *
- * If you have a ./config/autoload/ directory set up for your project, 
- * drop this config file in it and change the values as you wish.
- */
-$settings = [
-    /**
-     * Informations concernant cette application
-     */
-    'app_infos' => [
-        'nom'     => "SoDoct",
-        'desc'    => "SOutenance, Doctorat et Organisation du Circuit des Thèses",
-        'version' => "2.0.1",
-        'date'    => "27/11/2017",
-        'contact' => [
-            'mail' => "assistance-sodoct@unicaen.fr",
-            //'tel' => "01 02 03 04 05",
-        ],
-        'mentionsLegales'        => "http://www.unicaen.fr/acces-direct/mentions-legales/",
-        'informatiqueEtLibertes' => "http://www.unicaen.fr/acces-direct/informatique-et-libertes/",
-    ],
-    /**
-     * Période d'exécution de la requête de rafraîchissement de la session utilisateur, en millisecondes.
-     */
-    'session_refresh_period' => 0, // 0 <=> aucune requête exécutée
-];
 
-/**
- * You do not need to edit below this line
- */
 return [
-    'unicaen-app' => $settings,
+    'unicaen-app' => [
+        /**
+         * Informations concernant l'application.
+         */
+        'app_infos' => [
+            'nom'     => "SYGAL",
+            'desc'    => "SYstème de Gestion et d'Accompagnement doctoraL",
+            'version' => "0.0.0",
+            'date'    => "27/11/2017",
+            'contact' => [
+//                'mail' => "assistance-sygal@unicaen.fr",
+//                'tel' => "01 02 03 04 05",
+            ],
+            'mentionsLegales'        => "http://www.unicaen.fr/acces-direct/mentions-legales/",
+            'informatiqueEtLibertes' => "http://www.unicaen.fr/acces-direct/informatique-et-libertes/",
+        ],
+
+        /**
+         * Période d'exécution de la requête de rafraîchissement de la session utilisateur, en millisecondes.
+         */
+        'session_refresh_period' => 0, // 0 <=> aucune requête exécutée
+    ]
 ];
