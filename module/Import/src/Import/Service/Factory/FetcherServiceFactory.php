ff1172d860b05b52ed0ebb1b4159f4c3d3b86f05
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2018-02-23 15:33:28 +0100

    Changement des types d'exception

--- module/Import/src/Import/Service/Factory/FetcherServiceFactory.php
+++ module/Import/src/Import/Service/Factory/FetcherServiceFactory.php
@@ -5,6 +5,7 @@ namespace Import\Service\Factory;
 use Import\Service\FetcherService;
 use Doctrine\ORM\EntityManager;
 use Zend\ServiceManager\ServiceLocatorInterface as ContainerInterface;
+use UnicaenApp\Exception;
 
 class FetcherServiceFactory {
 
@@ -14,10 +15,10 @@ class FetcherServiceFactory {
         $config = $container->get('config');
 
         if (!isset($config['users']['login'])) {
-            throw new \Exception("La clef <strong>login</strong> est manquante dans le fichier de configuration local de l'Import");
+            throw new Exception\MandatoryValueException("La clef <strong>login</strong> est manquante dans le fichier de configuration local de l'Import");
         }
         if (!isset($config['users']['password'])) {
-            throw new \Exception("La clef <strong>password</strong> est manquante dans le fichier de configuration local de l'Import");
+            throw new Exception\MandatoryValueException("La clef <strong>password</strong> est manquante dans le fichier de configuration local de l'Import");
         }
 
         $service = new FetcherService($entityManager, $config);
