ad1d1cc9ae1213a785ab2034db8213f374cf8600
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-04-04 15:22:41 +0200

    SUppression script de deploiement des configs locales

--- deploy-configs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x # expands variables and prints a little + sign before the line
-
-appname="sygal"
-
-# preprod
-scp ./config/autoload/local.sygal-pp.php root@sygal-pp:/root/.ssh/${appname}/
