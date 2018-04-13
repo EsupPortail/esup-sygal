83d7b1882ecd99dde45e15c5710ec0353e4eeb02
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-04-13 10:00:18 +0200

    Roles : SQL utile

---
+++ data/SQL/role.sql
@@ -0,0 +1,32 @@
+--
+-- Nouveau rôle.
+--
+insert into role (
+  ID,
+  CODE,
+  LIBELLE,
+  SOURCE_CODE,
+  SOURCE_ID,
+  ROLE_ID,
+  STRUCTURE_ID,
+  TYPE_STRUCTURE_DEPENDANT_ID,
+  THESE_DEP,
+  HISTO_CREATEUR_ID,
+  HISTO_MODIFICATEUR_ID
+) SELECT
+  ROLE_ID_SEQ.nextval,
+  'OBSERV',
+  'Observateur',
+  'OBSERV',
+  src.id,
+  'Observateur COMUE',
+  null,
+  null,
+  0,
+  u.id,
+  u.id
+  from source src, UTILISATEUR u
+  where
+    src.CODE = 'COMUE::SYGAL' and
+    u.USERNAME = 'sygal-app'
+;
