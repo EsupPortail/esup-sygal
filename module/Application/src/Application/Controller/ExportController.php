9d4a65db7879a556bef6043127aec9b3a83b00d2
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-06-26 12:06:54 +0200

    Export CSV: correction erreur d'appel méthode supprimée ; application des filtres.

--- module/Application/src/Application/Controller/ExportController.php
+++ module/Application/src/Application/Controller/ExportController.php
@@ -5,26 +5,18 @@ namespace Application\Controller;
 use Application\Entity\Db\NatureFichier;
 use Application\Entity\Db\These;
 use Application\Service\Fichier\FichierServiceAwareTrait;
+use Application\Service\These\TheseRechercheServiceAwareTrait;
 use Application\Service\These\TheseServiceAwareTrait;
-use Application\View\Helper\Sortable;
 use UnicaenApp\View\Model\CsvModel;
 
 class ExportController extends AbstractController
 {
     use TheseServiceAwareTrait;
+    use TheseRechercheServiceAwareTrait;
     use FichierServiceAwareTrait;
 
     public function csvAction()
     {
-
-        //Generer la requete avec les filtres ...
-        /**
-         * @var \Doctrine\ORM\QueryBuilder      $qb
-         * @var \Doctrine\ORM\Query             $query
-         * @var These                           $these
-         */
-
-        //TODO compléter avec les infos demandées
         $headers = [
             // Doctorant
             'Civilité'                              => function (These $these) { return $these->getDoctorant()->getIndividu()->getCivilite(); },
@@ -62,28 +54,16 @@ class ExportController extends AbstractController
             'Annexe non PDF'                        => function (These $these) { if (!empty($this->fichierService->getRepository()->fetchFichiers($these, NatureFichier::CODE_FICHIER_NON_PDF))) return 'O'; else return 'N'; },
 
         ];
-        $etatThese = $this->params()->fromQuery('etatThese');
-        //$sort = $this->params()->fromQuery('sort');
-        $text = $this->params()->fromQuery('text');
-        $dir  = $this->params()->fromQuery('direction', Sortable::ASC);
-        $sort = $this->params()->fromQuery('sort');
 
-//        var_dump($etatThese);
-//        var_dump($sort);
-//        var_dump($text);
-//        var_dump($dir);
+        $queryParams = $this->params()->fromQuery();
 
-//        $repoThese = $this->theseService->getEntityManager()->getRepository(These::class);
-//        $qb = $repoThese->createQueryBuilder("t")
-//            ->where("t.ecoleDoctorale = 3")
-//            ->andWhere("t.etatThese = :etatThese")
-//        ;
-//        $qb->setParameter("etatThese", $etatThese);
-//        $query = $qb->getQuery();
-        $query = $this->createQueryBuilder()->getQuery();
+        $this->theseRechercheService
+            ->createFilters()
+            ->createSorters()
+            ->processQueryParams($queryParams);
 
-            //Execution de la requete
-        $theses = $query->execute();
+        $qb = $this->theseRechercheService->createQueryBuilder();
+        $theses = $qb->getQuery()->getResult();
 
         $records = [];
         for ($i = 0 ; $i < count($theses) ; $i++) {
@@ -103,57 +83,5 @@ class ExportController extends AbstractController
         $result->setFilename('export_theses.csv');
 
         return $result;
-    }
-
-    //TODO FACTORISER AVEC INDEX DES THESES ...
-    private function createQueryBuilder() {
-        $etatThese = $this->params()->fromQuery($name = 'etatThese');
-        $sort = $this->params()->fromQuery('sort');
-        $text = $this->params()->fromQuery('text');
-        $dir  = $this->params()->fromQuery('direction', Sortable::ASC);
-
-        $qb = $this->theseService->getRepository()->createQueryBuilder('t')
-            ->addSelect('di')->leftJoin('th.individu', 'di')
-            ->addSelect('ed')->leftJoin('t.ecoleDoctorale', 'ed')
-            ->addSelect('ur')->leftJoin('t.uniteRecherche', 'ur')
-            ->addSelect('a')->leftJoin('t.acteurs', 'a')
-            ->addSelect('i')->leftJoin('a.individu', 'i')
-            ->addSelect('r')->leftJoin('a.role', 'r')
-            ->andWhere('1 = pasHistorise(t)');
-
-        if ($etatThese) {
-            $qb->andWhere('t.etatThese = :etat')->setParameter('etat', $etatThese);
-        }
-        $sortProps = $sort ? explode('+', $sort) : [];
-        foreach ($sortProps as $sortProp) {
-            if ($sortProp === 't.titre') {
-                // trim et suppression des guillemets
-                $sortProp = "TRIM(REPLACE($sortProp, CHR(34), ''))"; // CHR(34) <=> "
-            }
-            $qb->addOrderBy($sortProp, $dir);
-        }
-
-        /**
-         * Filtres découlant du rôle de l'utilisateur.
-         */
-        $this->theseService->decorateQbFromUserContext($qb, $this->userContextService);
-
-        /**
-         * Prise en compte du champ de recherche textuelle.
-         */
-        if (strlen($text) > 1) {
-            $results = $this->theseService->rechercherThese($text);
-            $sourceCodes = array_unique(array_keys($results));
-            if ($sourceCodes) {
-                $qb
-                    ->andWhere($qb->expr()->in('t.sourceCode', ':sourceCodes'))
-                    ->setParameter('sourceCodes', $sourceCodes);
-            }
-            else {
-                $qb->andWhere("0 = 1"); // i.e. aucune thèse trouvée
-            }
-        }
-
-        return $qb;
     }
 }
