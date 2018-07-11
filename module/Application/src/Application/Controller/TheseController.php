d29f8bd9c77d44ae23139d382133bb0cf1a6112f
jean-philippe.metivier@unicaen.fr <jean-philippe.metivier@unicaen.fr>
2018-07-11 10:57:22 +0200

    Amélioration du formulaire 'points de vigilance' de la tache #15672

--- module/Application/src/Application/Controller/TheseController.php
+++ module/Application/src/Application/Controller/TheseController.php
@@ -1401,6 +1401,7 @@ class TheseController extends AbstractController
                     //message de notification dans la page
                     $message = "Les points de vigilance viennent d'être sauvegardés.";
                     $this->flashMessenger()->addSuccessMessage($message);
+                    $this->redirect()->toRoute('these/points-de-vigilance', ["these" => $these->getId()]);
                 }
             }
         }
