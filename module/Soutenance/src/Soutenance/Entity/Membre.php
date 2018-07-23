269eb02db8752485737f7a508ab09e0bb2389e78
jean-philippe.metivier@unicaen.fr <jean-philippe.metivier@unicaen.fr>
2018-07-23 10:30:52 +0200

    Calcul des indicateurs et validité du jury

---
+++ module/Soutenance/src/Soutenance/Entity/Membre.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Soutenance\Entity;
+
+class Membre {
+
+    /** @var int */
+    private $id;
+    /** @var Proposition */
+    private $proposition;
+    /** @var string */
+    private $genre;
+    /** @var string */
+    private $denomination;
+    /** @var string */
+    private $qualite;
+    /** @var string */
+    private $rang;
+    /** @var string */
+    private $etablissement;
+    /** @var string */
+    private $exterieur;
+    /** @var string */
+    private $role;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Proposition
+     */
+    public function getProposition()
+    {
+        return $this->proposition;
+    }
+
+    /**
+     * @param Proposition $proposition
+     * @return Membre
+     */
+    public function setProposition($proposition)
+    {
+        $this->proposition = $proposition;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * @param string $genre
+     * @return Membre
+     */
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDenomination()
+    {
+        return $this->denomination;
+    }
+
+    /**
+     * @param string $denomination
+     * @return Membre
+     */
+    public function setDenomination($denomination)
+    {
+        $this->denomination = $denomination;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQualite()
+    {
+        return $this->qualite;
+    }
+
+    /**
+     * @param string $qualite
+     * @return Membre
+     */
+    public function setQualite($qualite)
+    {
+        $this->qualite = $qualite;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRang()
+    {
+        return $this->rang;
+    }
+
+    /**
+     * @param string $rang
+     * @return Membre
+     */
+    public function setRang($rang)
+    {
+        $this->rang = $rang;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEtablissement()
+    {
+        return $this->etablissement;
+    }
+
+    /**
+     * @param string $etablissement
+     * @return Membre
+     */
+    public function setEtablissement($etablissement)
+    {
+        $this->etablissement = $etablissement;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getExterieur()
+    {
+        return $this->exterieur;
+    }
+
+    /**
+     * @param string $exterieur
+     * @return Membre
+     */
+    public function setExterieur($exterieur)
+    {
+        $this->exterieur = $exterieur;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $role
+     * @return Membre
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+        return $this;
+    }
+}
+
+?>
--- module/Soutenance/src/Soutenance/Entity/Proposition.php
+++ module/Soutenance/src/Soutenance/Entity/Proposition.php
@@ -17,19 +17,19 @@ class Proposition {
     /** @var string */
     private $lieu;
 
-//    /** @var ArrayCollection */
-//    private $membres;
+    /** @var ArrayCollection */
+    private $membres;
 //    /** @var ArrayCollection */
 //    private $validations;
 
     /**
      * Proposition constructor.
      */
-//    public function __construct()
-//    {
-//        $this->membres = new ArrayCollection();
+    public function __construct()
+    {
+        $this->membres = new ArrayCollection();
 //        $this->validations = new ArrayCollection();
-//    }
+    }
 
     /**
      * @return int
@@ -129,5 +129,70 @@ class Proposition {
         return $this;
     }
 
+
+    /** fonction metier qui crée un tableau associatif des indicateurs*/
+    public function computeIndicateur() {
+        $nbMembre       = 0;
+        $nbFemme        = 0;
+        $nbHomme        = 0;
+        $nbRangA        = 0;
+        $nbExterieur    = 0;
+        $nbRapporteur   = 0;
+
+        /** @var Membre $membre */
+        foreach ($this->getMembres() as $membre) {
+            $nbMembre++;
+            if ($membre->getGenre() === "F") $nbFemme++; else $nbHomme++;
+            if ($membre->getRang() === "A") $nbRangA++;
+            if ($membre->getExterieur() === "oui") $nbExterieur++;
+            if ($membre->getRole() === "Rapporteur") $nbRapporteur++;
+        }
+
+        $indicateurs = [];
+
+        /**  Il faut essayer de maintenir la parité Homme/Femme*/
+        $ratioFemme = $nbFemme / $nbMembre;
+        $ratioHomme = 1 - $ratioFemme;
+        $indicateurs["parité"]      = ["Femme" => $ratioFemme, "Homme" => $ratioHomme];
+        if ($ratioFemme < 0.2 OR $ratioHomme < 0.2) {
+            $indicateurs["parité"]["valide"]    = false;
+        } else {
+            $indicateurs["parité"]["valide"]    = true;
+        }
+
+        /** Au moins deux rapporteurs */
+        $indicateurs["rapporteur"]      = ["Nombre" => $nbRapporteur, "Ratio" => $nbRapporteur/$nbMembre];
+        if ($nbRapporteur < 2) {
+            $indicateurs["rapporteur"]["valide"]    = false;
+        } else {
+            $indicateurs["rapporteur"]["valide"]    = true;
+        }
+
+        /** Au moins la motié du jury de rang A */
+        $ratioRangA = $nbRangA / $nbMembre;
+        $indicateurs["rang A"]      = ["Nombre" => $nbRangA, "Ratio" => $ratioRangA];
+        if ($ratioRangA < 0.5)  {
+            $indicateurs["rang A"]["valide"]    = false;
+        } else {
+            $indicateurs["rang A"]["valide"]    = true;
+        }
+
+        /** Au moins la motié du jury exterieur*/
+        $ratioExterieur = $nbExterieur / $nbMembre;
+        $indicateurs["exterieur"]      = ["Nombre" => $nbExterieur, "Ratio" => $ratioExterieur];
+        if ($ratioRangA < 0.5)  {
+            $indicateurs["exterieur"]["valide"]    = false;
+        } else {
+            $indicateurs["exterieur"]["valide"]    = true;
+        }
+
+        $valide = $indicateurs["parité"]["valide"] && $indicateurs["rapporteur"]["valide"]
+            && $indicateurs["rang A"]["valide"] && $indicateurs["exterieur"]["valide"];
+
+        $indicateurs["valide"] = $valide;
+
+        return $indicateurs;
+
+    }
 
 }
