2bf539ee7319b32004f54baef1f54c0e9f8c87d2
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2018-10-02 12:05:33 +0200

    Amélioration des logs

--- module/Import/src/Import/Exception/CallException.php
+++ module/Import/src/Import/Exception/CallException.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Psr7;
 use UnicaenApp\Exception\RuntimeException;
+use UnicaenApp\Util;
 use Zend\Http\Response;
 
 class CallException extends RuntimeException
@@ -19,7 +20,6 @@ class CallException extends RuntimeException
     static public function badConfig(\Exception $previous = null)
     {
         return new self("La config fournie est incorrecte.", null, $previous);
-
     }
 
     static public function unexpectedResponse($uri, Response $response)
@@ -27,9 +27,12 @@ class CallException extends RuntimeException
         return new self("Mauvaise réponse du WS (URI '$uri') ! Status : " . $response->getStatusCode());
     }
 
-    static public function invalidJSON($uri)
+    static public function invalidJSONResponse($uri, $responseBody)
     {
-        return new self("Impossible de décoder la réponse JSON du WS (URI '$uri') : " . json_last_error_msg());
+        return new self(sprintf(
+            "Erreur rencontrée lors du décodage de la réponse JSON du WS (URI '%s') : %s. Début de la réponse reçue : %s",
+            $uri, json_last_error_msg(), Util::truncatedString($responseBody, 100)
+        ));
     }
 
     static public function clientError(ClientException $error)
