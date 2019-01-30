b9b2fbf7dcb465508acb909cff578271ea21d83c
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2019-01-30 15:31:15 +0100

    Nouveau fichier de config version.global.php généré par le script 'bump-version'.

--- config/autoload/version.global.php
+++ config/autoload/version.global.php
@@ -1,8 +1,10 @@
 <?php
-return array(
-    'unicaen-app' => array(
-        'app_infos' => array(
-            'version' => '1.0.4',
-        ),
-    ),
-);
+return [
+    'unicaen-app' => [
+        'app_infos' => [
+            'version' => '1.0.5',
+            'date' => '30/01/2019',
+        ],
+    ],
+    'comment' => 'Fichier généré le 30/01/2019 à 15:26:44 avec /home/gauthierb/workspace/sygal/bump-version',
+];
