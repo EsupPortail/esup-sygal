95fc4666f537dd5fefa9a6550c7a325dbad8d7b7
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2019-03-15 11:41:25 +0100

    nouvelle version

--- config/autoload/version.global.php
+++ config/autoload/version.global.php
@@ -2,9 +2,9 @@
 return [
     'unicaen-app' => [
         'app_infos' => [
-            'version' => '1.1.1',
-            'date' => '25/02/2019',
+            'version' => '1.1.2',
+            'date' => '15/03/2019',
         ],
     ],
-    'comment' => 'Fichier généré le 25/02/2019 à 13:34:10 avec /home/gauthierb/workspace/sygal/bump-version',
+    'comment' => 'Fichier généré le 15/03/2019 à 11:40:07 avec /home/metivier/MyWeb/sygal/bump-version',
 ];
