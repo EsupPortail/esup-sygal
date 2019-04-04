b8f86b3ff8bcb39ea8295df0fe4f08e05d6fda5e
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2019-04-04 17:05:47 +0200

    Correction des scripts de création de la bdd from scratch.

---
+++ doc/database/02-inserts/oracle-data-insert-from-SYGAL.DOMAINE_SCIENTIFIQUE-into-SYGAL.sql
@@ -0,0 +1,12 @@
+set define off ;
+
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (1, 'Mathématiques et leurs interactions') ;
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (2, 'Physique') ;
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (3, 'Sciences de la terre et de l''univers, espace') ;
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (4, 'Chimie') ;
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (5, 'Biologie, médecine et santé') ;
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (6, 'Sciences humaines et humanités') ;
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (7, 'Sciences de la société') ;
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (8, 'Sciences pour l''ingénieur') ;
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (9, 'Sciences et technologies de l''information et de la communication') ;
+INSERT INTO SYGAL.DOMAINE_SCIENTIFIQUE (ID, LIBELLE) VALUES (10, 'Sciences agronomiques et écologiques') ;
