3dba1c98a7d91d1d38d8080b92059c5d2dfdefc9
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2019-04-09 11:03:54 +0200

    Version 1.1.5

--- config/autoload/version.global.php
+++ config/autoload/version.global.php
@@ -2,9 +2,9 @@
 return [
     'unicaen-app' => [
         'app_infos' => [
-            'version' => '1.1.4',
-            'date' => '01/04/2019',
+            'version' => '1.1.5',
+            'date' => '09/04/2019',
         ],
     ],
-    'comment' => 'Fichier généré le 01/04/2019 à 12:13:35 avec /home/gauthierb/workspace/sygal/bump-version',
+    'comment' => 'Fichier généré le 09/04/2019 à 10:36:31 avec /home/gauthierb/workspace/sygal/bump-version',
 ];
