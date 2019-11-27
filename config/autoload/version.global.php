5647ef809aca97f9c6d98c308a4fcefe3e745992
Jean-Philippe Metivier <jean-philippe.metivier@unicaen.fr>
2019-11-27 13:40:32 +0100

    Version 1.3.2 et changement affichage des établissements

--- config/autoload/version.global.php
+++ config/autoload/version.global.php
@@ -2,9 +2,9 @@
 return [
     'unicaen-app' => [
         'app_infos' => [
-            'version' => '1.3.1',
-            'date' => '25/11/2019',
+            'version' => '1.3.2',
+            'date' => '27/11/2019',
         ],
     ],
-    'comment' => 'Fichier généré le 25/11/2019 à 14:40:27 avec /home/gauthierb/workspace/sygal/bump-version',
+    'comment' => 'Fichier généré le 27/11/2019 à 13:39:25 avec /home/metivier/MyWeb/sygal/bump-version',
 ];
