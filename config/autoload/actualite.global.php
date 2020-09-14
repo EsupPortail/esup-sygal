5316ae2d6f80635267f927e37083ee7cfdf37e22
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2020-09-14 13:44:05 +0200

    Config des actualités directement dans .local.php

--- config/autoload/actualite.global.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'actualite' => [
-        'flux' => "https://www.normandie-univ.fr/adminsite/webservices/export_rss.jsp?objet=actualite&TYPE_EVENEMENT=3ZZ125GT&THEMATIQUE=3WKWANK3&TRI_DATE=DATE_ASC&TYPE_FLUX_FEED=rss_2.0&DESCRIPTION=FluxSyGAL",
-    ],
-];
