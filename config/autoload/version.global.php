4a129c7885a624f78b2e4c5915f7e58cf9085421
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2020-10-02 16:01:00 +0200

    Numéro de version par défaut

--- config/autoload/version.global.php
+++ config/autoload/version.global.php
@@ -2,9 +2,8 @@
 return [
     'unicaen-app' => [
         'app_infos' => [
-            'version' => '1.4.9',
-            'date' => '14/09/2020',
+            'version' => '2.0.0RC',
+            'date' => '01/10/2020',
         ],
     ],
-    'comment' => 'Fichier généré le 14/09/2020 à 13:52:22 avec /home/gauthierb/workspace/sygal-zf2/bump-version',
 ];
