b02a325098989d54e57d7318b4fc4ce8533d7bd6
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2021-02-01 11:20:16 +0100

    Test d'archivabilité : possibilité de configurer le passage par un proxy.

---
+++ module/Application/src/Application/Entity/Db/VSitu/DepotVersionCorrigeeValidationPresident.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Application\Entity\Db\VSitu;
+
+/**
+ * DepotVersionCorrigeeValidationPresident
+ */
+class DepotVersionCorrigeeValidationPresident
+{
+    /**
+     * @var boolean
+     */
+    private $valide;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \Application\Entity\Db\These
+     */
+    private $these;
+
+    /**
+     * @var \Application\Entity\Db\Individu
+     */
+    private $individu;
+
+
+    /**
+     * Set valide
+     *
+     * @param boolean $valide
+     *
+     * @return DepotVersionCorrigeeValidationPresident
+     */
+    public function setValide($valide)
+    {
+        $this->valide = $valide;
+
+        return $this;
+    }
+
+    /**
+     * Get valide
+     *
+     * @return boolean
+     */
+    public function getValide()
+    {
+        return $this->valide;
+    }
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     *
+     * @return DepotVersionCorrigeeValidationPresident
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set these
+     *
+     * @param \Application\Entity\Db\These $these
+     *
+     * @return DepotVersionCorrigeeValidationPresident
+     */
+    public function setThese(\Application\Entity\Db\These $these = null)
+    {
+        $this->these = $these;
+
+        return $this;
+    }
+
+    /**
+     * Get these
+     *
+     * @return \Application\Entity\Db\These
+     */
+    public function getThese()
+    {
+        return $this->these;
+    }
+
+    /**
+     * Set individu
+     *
+     * @param \Application\Entity\Db\Individu $individu
+     *
+     * @return DepotVersionCorrigeeValidationPresident
+     */
+    public function setIndividu(\Application\Entity\Db\Individu $individu = null)
+    {
+        $this->individu = $individu;
+
+        return $this;
+    }
+
+    /**
+     * Get individu
+     *
+     * @return \Application\Entity\Db\Individu
+     */
+    public function getIndividu()
+    {
+        return $this->individu;
+    }
+}
+
