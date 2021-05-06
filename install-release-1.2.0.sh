08c0281c264b82e16f70c455f74bd6b735330314
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2021-05-06 10:52:03 +0200

    Suppression de fichiers obsolètes

--- install-release-1.2.0.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#
-# DB upgrade
-#
-echo "Mise à jour de la base de données..."
-#php public/index.php run-sql-script --path=./data/SQL/release-1.2.0/10_fix.sql && \
-php public/index.php run-sql-script --path=./data/SQL/release-1.2.0/20_refonte-fichier.sql && \
-echo ":-) Base de données mise à jour avec succès !"
