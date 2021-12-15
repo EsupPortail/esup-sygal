f42e14553bd0c30616a6ffc35f2198b80c94158c
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2021-12-15 17:42:46 +0100

    Utilisation du nouveau ShellCommandRunnerTrait

---
+++ module/Application/src/Application/Command/ShellCommandRunnerTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Application\Command;
+
+use UnicaenApp\Exception\RuntimeException;
+
+trait ShellCommandRunnerTrait
+{
+    /**
+     * @throws \Application\Command\Exception\TimedOutCommandException
+     */
+    protected function runShellCommand(ShellCommandInterface $command, string $timeout = null)
+    {
+        $runner = new ShellCommandRunner();
+        $runner->setCommand($command);
+        try {
+            if ($timeout) {
+                $result = $runner->runCommandWithTimeout($timeout);
+            } else {
+                $result = $runner->runCommand();
+            }
+
+            if (!$result->isSuccessfull()) {
+                $message = sprintf("La commande '%s' a échoué (code retour = %s). ",
+                    $command->getName(),
+                    $result->getReturnCode()
+                );
+                if ($output = $result->getOutput()) {
+                    $message .= "Voici le log d'exécution : " . implode(PHP_EOL, $output);
+                }
+                throw new RuntimeException($message);
+            }
+        }
+        catch (RuntimeException $rte) {
+            throw new RuntimeException(
+                "Une erreur est survenue lors de l'exécution de la commande " . $command->getName(),
+                0,
+                $rte);
+        }
+
+    }
+}
