0275c7fe790b0e07af2a8ecc0bbd1ce7f0ec8256
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2022-10-18 11:45:55 +0200

    Réorganisation de install.sh pour garantir que les commandes Doctrine ne plantent pas à cause de proxies obsolètes

--- install.sh
+++ install.sh
@@ -20,13 +20,16 @@ mkdir -p upload && \
 # Composer install
 composer install --no-interaction --no-suggest --prefer-dist --optimize-autoloader
 
+# Création ou vidange des répertoires de cache
+mkdir -p data/cache && chmod -R 777 data/cache && rm -rf data/cache/*
+mkdir -p data/DoctrineModule/cache && chmod -R 777 data/DoctrineModule/cache && rm -rf data/DoctrineModule/cache/*
+mkdir -p data/DoctrineORMModule/Proxy && chmod -R 777 data/DoctrineORMModule/Proxy && rm -rf data/DoctrineORMModule/Proxy/*
+
+vendor/bin/laminas-development-mode enable
+
 vendor/bin/doctrine-module orm:generate-proxies
 vendor/bin/doctrine-module orm:clear-cache:query
 vendor/bin/doctrine-module orm:clear-cache:metadata
 vendor/bin/doctrine-module orm:clear-cache:result
 
 vendor/bin/laminas-development-mode disable
-
-# Création ou vidange des répertoires de cache
-mkdir -p data/cache && chmod -R 777 data/cache && rm -rf data/cache/*
-mkdir -p data/DoctrineModule/cache && chmod -R 777 data/DoctrineModule/cache && rm -rf data/DoctrineModule/cache/*
