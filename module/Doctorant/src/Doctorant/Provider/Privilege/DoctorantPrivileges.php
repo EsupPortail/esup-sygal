2dda4585e29d390392267331c0bb48c722272a1d
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2023-11-20 10:32:27 +0100

    Ajout de futurs privilèges Doctorant, pas encore exploités.

--- module/Doctorant/src/Doctorant/Provider/Privilege/DoctorantPrivileges.php
+++ module/Doctorant/src/Doctorant/Provider/Privilege/DoctorantPrivileges.php
@@ -6,6 +6,10 @@ use UnicaenAuth\Provider\Privilege\Privileges;
 
 class DoctorantPrivileges extends Privileges
 {
+    const DOCTORANT_LISTER_TOUT = 'doctorant-lister-tout'; // todo : existe en bdd mais pas encore exploité
+    const DOCTORANT_LISTER_SIEN = 'doctorant-lister-sien'; // todo : existe en bdd mais pas encore exploité
+    const DOCTORANT_CONSULTER_TOUT = 'doctorant-consulter-tout'; // todo : existe en bdd mais pas encore exploité
+    const DOCTORANT_CONSULTER_SIEN = 'doctorant-consulter-sien'; // todo : existe en bdd mais pas encore exploité
     const DOCTORANT_AFFICHER_EMAIL_CONTACT          = 'doctorant-afficher-email-contact';
     const DOCTORANT_MODIFIER_EMAIL_CONTACT          = 'doctorant-modifier-email-contact';
 //    const DOCTORANT_MODIFIER_EMAIL_CONTACT_CONSENT  = 'doctorant-modifier-email-contact-consent';
