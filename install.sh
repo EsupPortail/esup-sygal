9df3ed52f1aed313cedb5a3d7ac3420b4bec572c
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2024-05-15 09:05:40 +0200

    Revert "install.sh : réorganisation ; plus de rm -rf data/DoctrineModule/cache/* ; tentative de ne plus vider le cache de résultat."
    This reverts commit 5483c0ba7b6bd38057cb99f0f73fc37502fc2dd1.

--- install.sh
+++ install.sh
@@ -17,19 +17,19 @@ mkdir -p upload && \
   chown -R www-data:root upload && \
   chmod -R 770 upload
 
-# Installation des dépendances PHP
+# Composer install
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
-# Cache Laminas
-mkdir -p data/cache && chmod 777 data/cache
-rm -rf data/cache/*
-
-# Cache Doctrine
-mkdir -p data/DoctrineModule/cache && chmod 777 data/DoctrineModule/cache #&& rm -rf data/DoctrineModule/cache/*
+# Création ou vidange des répertoires de cache
+mkdir -p data/cache && chmod 777 data/cache && rm -rf data/cache/*
+mkdir -p data/DoctrineModule/cache && chmod 777 data/DoctrineModule/cache && rm -rf data/DoctrineModule/cache/*
 mkdir -p data/DoctrineORMModule/Proxy && chmod 777 data/DoctrineORMModule/Proxy && rm -rf data/DoctrineORMModule/Proxy/*
-vendor/bin/laminas-development-mode enable  # nécessaire !
+
+vendor/bin/laminas-development-mode enable
+
 vendor/bin/doctrine-module orm:generate-proxies
 vendor/bin/doctrine-module orm:clear-cache:query
 vendor/bin/doctrine-module orm:clear-cache:metadata
-#vendor/bin/doctrine-module orm:clear-cache:result  ### tentative de ne plus vider le cache de résultat
+vendor/bin/doctrine-module orm:clear-cache:result
+
 vendor/bin/laminas-development-mode disable
