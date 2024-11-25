4cb31e20e33ed93ddb3c2ac65e0af485984736e5
Bertrand Gauthier <bertrand.gauthier@unicaen.fr>
2024-11-25 12:12:53 +0100

    Mise à jour du SQL pour la v9.2.0

--- doc/release-notes/v9/9.2.0/02_parametre.sql
+++ doc/release-notes/v9/9.2.0/02_parametre.sql
@@ -8,3 +8,12 @@
 
 alter table unicaen_parametre_parametre add column affichable boolean not null default true;
 alter table unicaen_parametre_parametre add column modifiable boolean not null default true;
+
+INSERT INTO unicaen_privilege_privilege(CATEGORIE_ID, CODE, LIBELLE, ORDRE)
+WITH d(code, lib, ordre) AS (
+    SELECT 'parametre_afficher_masquer', 'Affichage de les paramètres masqués', 15
+)
+SELECT cp.id, d.code, d.lib, d.ordre
+FROM d JOIN unicaen_privilege_categorie cp ON cp.CODE = 'parametre';
+
+select privilege__grant_privilege_to_profile('parametre', 'parametre_afficher_masquer', 'ADMIN_TECH');
