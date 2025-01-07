d783ed900e75113b9aaad1e1a79e03f4bd7e772f
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2025-01-07 15:47:35 +0100

    Correction du SQL de la v9.2.0

--- doc/release-notes/v9/9.2.0/10_templates.sql
+++ doc/release-notes/v9/9.2.0/10_templates.sql
@@ -41,9 +41,6 @@ update unicaen_renderer_template set document_corps = replace(document_corps, '[
                                      document_sujet = replace(document_sujet, '[TypeValidation#Libelle', '[AdmissionOperation#LibelleType')
                                  where document_corps like '%TypeValidation#Libelle%';
 
-INSERT INTO unicaen_renderer_macro (code, description, variable_name, methode_name)
-VALUES ('AdmissionOperation#LibelleType', '<p>Retourne le libellé du type d''opération (validation, avis)</p>', 'admissionOperation', 'getLibelleType');
-
 
 update unicaen_renderer_macro set code = 'SoutenanceMembre#Denomination',
                                   variable_name = 'soutenanceMembre',
@@ -62,5 +59,5 @@ update unicaen_renderer_template set document_corps = replace(document_corps, '[
 where document_corps like '%Membre#MembresPouvantEtrePresidentDuJuryAsUl%';
 
 
-select * from unicaen_renderer_template where code = 'ADMISSION_OPERATION_ATTENDUE';
-select * from unicaen_renderer_macro where variable_name = 'admissionOperation';
+-- select * from unicaen_renderer_template where code = 'ADMISSION_OPERATION_ATTENDUE';
+-- select * from unicaen_renderer_macro where variable_name = 'admissionOperation';
