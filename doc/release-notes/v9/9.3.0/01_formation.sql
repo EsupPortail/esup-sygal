0c6e9c3c4eb771ed0566fbe32e55a6f0d21e3393
thomas.hamel <thomas.hamel@unicaen.fr>
2025-01-21 09:29:52 +0100

    UX: affichage d'un message à l'utilisateur lui indiquant lors d'une suppression qu'une confirmation lui sera demandée avant suppression complète

--- doc/release-notes/v9/9.3.0/01_formation.sql
+++ doc/release-notes/v9/9.3.0/01_formation.sql
@@ -1,2 +1,6 @@
 alter table formation_formation add fiche_id bigint references fichier (id);
 INSERT INTO nature_fichier (id, code, libelle) VALUES (nextval('nature_fichier_id_seq'::regclass), 'FORMATION_FICHE', 'Fiche PDF détaillée d''une formation');
+
+update formation_etat set description = 'La session est imminente' where code = 'I';
+update formation_etat set description = 'Les inscriptions sont ouvertes' where code = 'O';
+update formation_etat set description = 'Les inscriptions sont à présent fermées' where code = 'F'
