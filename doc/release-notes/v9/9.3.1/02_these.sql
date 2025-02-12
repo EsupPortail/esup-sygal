a8a183a9efe6f40a28b8a37dc9431fc9311e4cb6
Bertrand GAUTHIER <bertrand.gauthier@unicaen.fr>
2025-02-12 10:42:18 +0100

    Correction script SQL de la 9.3.1

--- doc/release-notes/v9/9.3.1/02_these.sql
+++ doc/release-notes/v9/9.3.1/02_these.sql
@@ -2,6 +2,9 @@
 -- 9.3.1
 --
 
+call unicaen_indicateur_delete_matviews();
+
 -- select matviews__check_column_dropable(column_name := 'code_sise_disc');
 alter table these drop column code_sise_disc;
 
+call unicaen_indicateur_recreate_matviews();
